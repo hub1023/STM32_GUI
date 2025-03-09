1ced974af467f11330fe31d83ee9cc8a0b19990d
hubIT1023 <hubITtoday@gmail.com>
2025-03-10 01:32:49 +1300

    Add project files.

---
+++ STM32_GUI/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace STM32_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.m1Strat = new System.Windows.Forms.Button();
+            this.m1Stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM9";
+            // 
+            // m1Strat
+            // 
+            this.m1Strat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.m1Strat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m1Strat.Location = new System.Drawing.Point(26, 42);
+            this.m1Strat.Name = "m1Strat";
+            this.m1Strat.Size = new System.Drawing.Size(71, 47);
+            this.m1Strat.TabIndex = 0;
+            this.m1Strat.Text = "START";
+            this.m1Strat.UseVisualStyleBackColor = false;
+            this.m1Strat.Click += new System.EventHandler(this.m1Strat_Click);
+            // 
+            // m1Stop
+            // 
+            this.m1Stop.BackColor = System.Drawing.Color.Red;
+            this.m1Stop.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m1Stop.Location = new System.Drawing.Point(123, 42);
+            this.m1Stop.Name = "m1Stop";
+            this.m1Stop.Size = new System.Drawing.Size(71, 50);
+            this.m1Stop.TabIndex = 1;
+            this.m1Stop.Text = "STOP";
+            this.m1Stop.UseVisualStyleBackColor = false;
+            this.m1Stop.Click += new System.EventHandler(this.m1Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.m1Stop);
+            this.Controls.Add(this.m1Strat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button m1Strat;
+        private System.Windows.Forms.Button m1Stop;
+    }
+}
+
---
+++ STM32_GUI/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STM32_GUI
+{
+    public partial class Form1 : Form
+    {
+        private StringBuilder serialBuffer = new StringBuilder();
+        private Dictionary<string, Action<string>> commandHandlers;
+
+
+        public Form1()
+        {
+
+            try
+            {
+                InitializeComponent();
+
+                // Ensure serialPort1 is not null before opening
+                if (serialPort1 != null && !serialPort1.IsOpen)
+                {
+                    serialPort1.Open(); // Open the serial port safely
+                }
+
+                // Initialize Serial Port
+                serialPort1.DataReceived += serialPort1_DataReceived;
+
+                // Initialize Command Handlers
+                commandHandlers = new Dictionary<string, Action<string>>(StringComparer.OrdinalIgnoreCase)
+                {
+                   // { "cmd:htr_1", data => UpdateIndicator(Color.Lime) },
+                    //{ "cmd:htr_0", data => UpdateIndicator(Color.Red) },
+                    //{ "TEMP", data => ParseAndUpdateTempHumidity(data) }
+                };
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening serial port: " + ex.Message);
+            }
+        }
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string indata = serialPort1.ReadExisting().Trim();
+                if (!string.IsNullOrEmpty(indata))
+                {
+                    serialBuffer.Append(indata);
+
+                    // Ensure complete message processing (assuming newline `\n` as delimiter)
+                    if (indata.Contains("\n"))
+                    {
+                        string fullMessage = serialBuffer.ToString().Trim();
+                        serialBuffer.Clear(); // Clear buffer for next message
+
+                        Console.WriteLine("Received Data: " + fullMessage);
+                       // ProcessReceivedData(fullMessage);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                BeginInvoke(new Action(() => MessageBox.Show($"Serial Read Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void SendCommand(string command)
+        {
+            if (serialPort1 != null && serialPort1.IsOpen)
+            {
+                try
+                {
+                    string formattedCommand = command + "\r\n";  // Ensure STM32 gets CR+LF
+                    serialPort1.Write(formattedCommand);
+                    Console.WriteLine("Sent: " + formattedCommand);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error sending command: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Serial port is not open!");
+            }
+        }
+
+        private void m1Strat_Click(object sender, EventArgs e)
+        {
+           
+            SendCommand("PA_15:ON"); // PinID:State
+        }
+
+        private void m1Stop_Click(object sender, EventArgs e)
+        {
+           
+            SendCommand("PA_15:OFF"); // PinID:State
+        }
+        
+    }
+}
